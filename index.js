e0e9ee74a0a43b59439bf993efe8d6e8054531c5
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-02-13 14:26:59 -0600

    newsletter service

---
+++ index.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const { neon } = require("@neondatabase/serverless");
+
+const app = express();
+const PORT = process.env.PORT || 4242;
+
+app.use(express.json());
+
+app.use(cors());
+
+const sql = neon(`${process.env.DATABASE_URL}`);
+
+app.get("/ping", async (_, res) => {
+  res.json("👍");
+});
+
+// Read all components
+app.get("/subscribers", async (_, res) => {
+  try {
+    const query = `SELECT * FROM suscriptores;`;
+
+    const rows = await sql(query);
+
+    res.json(rows);
+  } catch (error) {
+    console.error("Error fetching components:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { nombre, correo, telefono, mensaje, origen } = req.body;
+
+    if (!nombre || !correo) {
+      return res.status(400).json({ error: "Nombre y email son requeridos" });
+    }
+
+    const query = `INSERT INTO suscriptores (nombre, correo, telefono, mensaje, origen)  
+                     VALUES ($1, $2, $3, $4, $5) RETURNING *;`;
+
+    const values = [nombre, correo, telefono, mensaje, origen];
+    const result = await sql(query, values);
+
+    res.status(201).json(result);
+  } catch (error) {
+    console.error("Error inserting subscriber:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to http://localhost:${PORT}`);
+});
